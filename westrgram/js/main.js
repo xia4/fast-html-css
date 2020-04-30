fa0d7e7407e01c57d71748d9b48ef433361d5c26
Kjintae <wlsxo2112@naver.com>
2020-04-30 16:48:35 +0900

    main page comment complete

--- westrgram/js/main.js
+++ westrgram/js/main.js
@@ -5,17 +5,27 @@ const submitButton = document.querySelector(".btn span");
 function commentUpdate() {
         const ulbox = document.querySelector(".comments_info.comment_list_ul")
         const liBox =  document.createElement("li");
-        const pBox =  document.createElement("p");
-        
-        pBox.setAttribute("class", "comment_tit");
-        pBox.innerHTML = comments.value;
+        const pBox =  document.createElement("div");
+        const userBox = document.createElement("span");
+        const commentCtnBox = document.createElement("span");
+        let user ="test";
+
+
+        userBox.setAttribute("class", "user_id");
+        commentCtnBox.setAttribute("class", "comment_contents");
+        pBox.setAttribute("class", "comments_tit");
+        userBox.innerHTML = user;
+        commentCtnBox.innerHTML = comments.value;
+       
+        pBox.appendChild(userBox);
+        pBox.appendChild(commentCtnBox);
         liBox.appendChild(pBox);
         ulbox.appendChild(liBox);
         comments.value = "";
 };
 
 
-// keydown시 page reload stop
+// keydown시 page stop
 commentForm.addEventListener("submit", function(e) {
     e.preventDefault();
 });
@@ -25,13 +35,22 @@ comments.addEventListener("keydown", function(e) {
     if(e.keyCode === 13) {
         const ulbox = document.querySelector(".comments_info.comment_list_ul")
         const liBox =  document.createElement("li");
-        const pBox =  document.createElement("p");
-        
-        pBox.setAttribute("class", "comment_tit");
-        pBox.innerHTML = comments.value;
+        const pBox =  document.createElement("div");
+        const userBox = document.createElement("span");
+        const commentCtnBox = document.createElement("span");
+        let user ="test";
+
+
+        userBox.setAttribute("class", "user_id");
+        commentCtnBox.setAttribute("class", "comment_contents");
+        pBox.setAttribute("class", "comments_tit");
+        userBox.innerHTML = user;
+        commentCtnBox.innerHTML = comments.value;
+       
+        pBox.appendChild(userBox);
+        pBox.appendChild(commentCtnBox);
         liBox.appendChild(pBox);
         ulbox.appendChild(liBox);
-    
         comments.value = "";
     }
 });
