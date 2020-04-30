ce17d16049c50f591863374766340bb7459edd43
Kjintae <wlsxo2112@naver.com>
2020-04-30 11:09:58 +0900

    main comment keydown

--- westrgram/js/main.js
+++ westrgram/js/main.js
@@ -1,33 +1,46 @@
+const commentForm = document.querySelector('.comments_form');
 const comments = document.querySelector("#comment_input");
 const submitButton = document.querySelector(".btn span");
 
-// 댓글 
-submitButton.addEventListener("click", function() {
-    const ulbox = document.querySelector(".comments_info.comment_list_ul")
-    const liBox =  document.createElement("li");
-    const pBox =  document.createElement("p");
-    //const spanBox = document.createElement("span");
+function commentUpdate() {
+        const ulbox = document.querySelector(".comments_info.comment_list_ul")
+        const liBox =  document.createElement("li");
+        const pBox =  document.createElement("p");
+        
+        pBox.setAttribute("class", "comment_tit");
+        pBox.innerHTML = comments.value;
+        liBox.appendChild(pBox);
+        ulbox.appendChild(liBox);
+        comments.value = "";
+};
 
-    pBox.setAttribute("class", "comment_tit");
-   //spanBox.setAttribute("class", "user_id");
 
-    //spanBox.innerHTML =user
-    pBox.innerHTML = comments.value;
-    //pBox.appendChild(spanBox);
-    liBox.appendChild(pBox);
-    ulbox.appendChild(liBox);
-
-    comments.value = "";
-    
+// keydown시 page reload stop
+commentForm.addEventListener("submit", function(e) {
+    e.preventDefault();
 });
 
-// event 두개를 넣고 function을 따로 빼서 그걸 콜백으로 넣어준다.
+submitButton.addEventListener("click",commentUpdate);
+comments.addEventListener("keydown", function(e) {
+    if(e.keyCode === 13) {
+        const ulbox = document.querySelector(".comments_info.comment_list_ul")
+        const liBox =  document.createElement("li");
+        const pBox =  document.createElement("p");
+        
+        pBox.setAttribute("class", "comment_tit");
+        pBox.innerHTML = comments.value;
+        liBox.appendChild(pBox);
+        ulbox.appendChild(liBox);
+    
+        comments.value = "";
+    }
+});
 
 //댓글 버튼 disabled
 comments.addEventListener("keyup" , function(e) {
     let commentValue = comments.value;
 
-    if(commentValue > 0) {
+    if(commentValue.length > 0) {
         submitButton.disabled = false;
         submitButton.style.cursor = "pointer";
         submitButton.style.color = "#1c7ed6";
